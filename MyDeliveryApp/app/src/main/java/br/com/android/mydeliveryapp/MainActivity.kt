1f569d10fab98d6b16993b79aeca6966ad615cfc
ramonhome <rmluzz@gmail.com>
2023-04-10 23:22:07 -0300

    Initial commit

---
+++ MyDeliveryApp/app/src/main/java/br/com/android/mydeliveryapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package br.com.android.mydeliveryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.android.mydeliveryapp.ui.theme.MyDeliveryAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyDeliveryAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyDeliveryAppTheme {
+        Greeting("Android")
+    }
+}
