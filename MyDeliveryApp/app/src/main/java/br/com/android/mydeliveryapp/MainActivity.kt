fc9ba7ca1ae0de3f16493746bc2e3f57ffe4b38b
Ramon Home <ramonhome@MacBook-Pro-de-Ramon.local>
2023-06-19 08:03:19 -0300

    Defining product compononents

--- MyDeliveryApp/app/src/main/java/br/com/android/mydeliveryapp/MainActivity.kt
+++ MyDeliveryApp/app/src/main/java/br/com/android/mydeliveryapp/MainActivity.kt
@@ -3,13 +3,24 @@ package br.com.android.mydeliveryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.android.mydeliveryapp.ui.theme.MyDeliveryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                   ProductItem()
                 }
             }
         }
@@ -41,3 +52,24 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+//@Preview(showBackground = true)
+@Composable
+fun ProductItem() {
+    Column {
+        Box(
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .background(Color.Blue)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "image item"
+        )
+
+        Text(text = "Text 1")
+        Text(text = "Text 2")
+    }
+}
+
